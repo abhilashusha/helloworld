9c1cbd6a73328bba11c9bc8f9621cf11922184eb
Abhilash Usha <abhilash.usha@Abhilashs-MacBook-Pro.local>
2021-11-15 15:24:46 -0500

    Changes

---
+++ scripts/deploy.sh
@@ -0,0 +1,46 @@
+set -x -e
+
+aws lambda get-alias \
+     --function-name $DEPLOY_FUNCTION_NAME \
+     --name $DEPLOY_ALIAS_NAME \
+     > output.json
+     DEVELOPMENT_ALIAS_VERSION= $( cat output.json | jq -r ;.FunctionVersion)
+
+cd function
+zip ../function.zip *
+cd ..
+
+aws lambda update-function-code \
+   --function-name $DEPLOY FUNCTION_NAME \
+   --zip-file fileb://function.zip \
+   --publish \
+   > output.json
+LATEST_VERSION  = $(cat output.json | jq -r '.Version')
+
+if [[ $DEVELOPMENT_ALIAS_VERSION -ge $LATEST_VERSION ]]; then
+    exit 0
+fi
+
+cat > $DEPLOY_APPSPEC_FILE <<- EOM
+version 0.0
+Resources:
+  -myLambdaFunction:
+     Type: AWS::Lambda::Function
+     Properties:
+       Name : "$DEPLOY_FUNCTION_NAME"
+       Alias:  "$DEPLOY_ALIAS_NAME"
+       CurrentVersion:  "$DEVELOPMENT_ALIAS_VERSION"
+       TargetVersion: "$LATEST_VERSION"
+EOM
+aws s3 cp \
+    $DEPLOY_APPSPEC_FILE \
+    s3://$DEPLOY_BUCKET_NAME/$DEPLOY_APPSPEC_FILE
+
+REVISION=revisionType=S3,s3Location={bucket=$DEPLOY_BUCKET_NAME,key=}
+
+aws lambda create-deployment \
+   --application-name $DEPLOY_APPLICATION_NAME \
+   --deployment-group-name $DEPLOYE_DEPLOYMENT_GROUP_NAME 
+   --deployment-config-name CodeDeployDefault.LambdaAllAtOnce \
+   --revision $REVISION
+
