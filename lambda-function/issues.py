6eee21819950073a8516a852f43984729081bc44
Abhilash Usha <abhilash.usha@Abhilashs-MacBook-Pro.local>
2021-11-18 08:11:03 -0500

    Removed Issues.py

--- lambda-function/issues.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-def transcode_file(request, filename):
-    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
-    subprocess.call(command, shell=True)  # a bad idea!
