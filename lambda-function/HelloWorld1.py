f3a2ca23db9b0a89247559b528cc6cdf3bdefe13
Abhilash Usha <abhilash.usha@Abhilashs-MacBook-Pro.local>
2021-11-17 20:39:00 -0500

    Corrected issues.py

--- lambda-function/HelloWorld1.py
+++ lambda-function/HelloWorld1.py
@@ -3,6 +3,22 @@ import json
 def lambda_handler(event, context):
     print(event)
     # TODO implement
+    print(" We will try to add new lines")
+    print(" We will try to add new lines1")
+    print(" We will try to add new lines2")
+    print(" We will try to add new lines3")
+    print(" We will try to add new lines4")
+    print(" We will try to add new lines5")
+    print(" We will try to add new lines6")
+    print(" We will try to add new lines7")
+    print(" We will try to add new lines8")
+    print(" We will try to add new lines9")
+    print(" We will try to add new lines10")
+    print(" We will try to add new lines11")
+    print(" We will try to add new lines12")
+
+
+
     return {
         'statusCode': 200,
         'body': json.dumps(' This is a new Version and the version is 11')
