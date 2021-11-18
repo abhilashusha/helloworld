ba2d9a06b876be76c27eb1269091a02bc735897b
Abhilash Usha <abhilash.usha@Abhilashs-MacBook-Pro.local>
2021-11-17 20:49:45 -0500

    Corrected issues.py

--- lambda-function/HelloWorld1.py
+++ lambda-function/HelloWorld1.py
@@ -17,7 +17,12 @@ def lambda_handler(event, context):
     print(" We will try to add new lines11")
     print(" We will try to add new lines12")
     print(" We will try to add new lines123")
-
+    print(" We will try to add new lines11")
+    print(" We will try to add new lines12")
+    print(" We will try to add new lines123") 
+    print(" We will try to add new lines11")
+    print(" We will try to add new lines12")
+    print(" We will try to add new lines123")
 
     return {
         'statusCode': 200,
