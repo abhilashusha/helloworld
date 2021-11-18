986f688981461804ba8a0018d87bcfe0a1ff145b
Abhilash Usha <abhilash.usha@Abhilashs-MacBook-Pro.local>
2021-11-17 20:24:44 -0500

    Status

--- lambda-function/issue.py
+++ lambda-function/issue.py
@@ -2,4 +2,3 @@ import subprocess
 
 def transcode_file(request, filename):
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
-    subprocess.call(command, shell=True)  # a bad idea!
