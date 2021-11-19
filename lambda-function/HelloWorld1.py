2ae4133e54c6092991acfe5b9adf8fc439b09dc0
abhilashusha <abhilash.u.nair@gmail.com>
2021-11-19 17:51:44 -0500

    latest changes

--- lambda-function/HelloWorld1.py
+++ lambda-function/HelloWorld1.py
@@ -3,27 +3,8 @@ import json
 def lambda_handler(event, context):
     print(event)
     # TODO implement
-    print(" We will try to add new lines")
-    print(" We will try to add new lines1")
-    print(" We will try to add new lines2")
-    print(" We will try to add new lines3")
-    print(" We will try to add new lines4")
-    print(" We will try to add new lines5")
-    print(" We will try to add new lines6")
-    print(" We will try to add new lines7")
-    print(" We will try to add new lines8")
-    print(" We will try to add new lines9")
-    print(" We will try to add new lines10")
-    print(" We will try to add new lines11")
-    print(" We will try to add new lines12")
-    print(" We will try to add new lines123")
-    print(" We will try to add new lines11")
-    print(" We will try to add new lines12")
-    print(" We will try to add new lines123") 
-    print(" We will try to add new lines11")
-    print(" We will try to add new lines12")
-    print(" We will try to add new lines123")
-    print(" We will try to add new lines123")
+    print(" We will try to add new lines 11")
+
 
     return {
         'statusCode': 200,
